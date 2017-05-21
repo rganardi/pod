ea0b72e66da4e6d7b48a6af39fb8ef8bc2d65c9f
Ray Ganardi <ray.fellix@gmail.com>
2017-05-21 17:41:13 +0800

    downloader done

--- pod.go
+++ pod.go
@@ -5,7 +5,10 @@ import (
 	"bytes"
 	"encoding/xml"
 	"os"
+	"path"
+	"io"
 	"io/ioutil"
+	"net/http"
 )
 
 type Query struct {
@@ -36,15 +39,71 @@ type EpisodeUrl struct {
 	Link	string `xml:"url,attr"`
 }
 
+func die(status int) {
+	if status > 0 {
+		os.Exit(1)
+	} else {
+		os.Exit(0)
+	}
+}
 
-func main() {
-	inputFile := os.Args[1]
-	fmt.Printf("hi!\n")
+func usage(status int) {
+	fmt.Print(`pod - small podcast thing
 
-	xmlFile, err := os.Open(inputFile)
+	pod command arguments
+
+available commands are
+	list			list all available podcast
+	info PODCAST		print info about PODCAST
+	fetch PODCAST		get the latest episode of PODCAST
+	pull			get the latest episode of all podcasts
+	clean PODCAST		remove media files for PODCAST
+	help			display this help
+`)
+	die(status)
+}
+
+func fetch(url, fileName string) {
+	fmt.Printf("downloading %s\r", url)
+
+	output, err := os.Create(fileName)
 	if err != nil {
-		fmt.Printf("can't open file %T\n", err)
-		return
+		fmt.Println(err)
+		die(1)
+	}
+	defer output.Close()
+
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println("error while downloading", url, "\n", err)
+		die(1)
+	}
+	defer response.Body.Close()
+
+	n, err := io.Copy(output, response.Body)
+
+	fmt.Printf("downloaded %s\t %v bytes\n", url, n)
+	return
+}
+
+func list() {
+	files, err := ioutil.ReadDir("rss")
+	if err != nil {
+		fmt.Println("%v\n", err)
+		die(1)
+	}
+	
+	for _, file := range files {
+		fmt.Printf("%v\n", file.Name())
+	}
+	die(0)
+}
+
+func podinfo(filename string) {
+	xmlFile, err := os.Open(filename)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		die(1)
 	}
 
 	defer xmlFile.Close()
@@ -58,16 +117,198 @@ func main() {
 	err = d.Decode(&q)
 	c := q.Podcast
 
-	fmt.Printf("%v\t%v\n", c.Title, c.Desc)
+	fmt.Printf("title\t%v\n", c.Title)
 
 	for _, feedurl := range c.NewFeedUrl {
 		if feedurl.Rel == "self" {
-			fmt.Printf("%v\n", feedurl.Link)
+			fmt.Printf("url\t%v", feedurl.Link)
+		}
+	}
+	fmt.Printf(`
+desc	%v
+`, c.Desc)
+	die(0)
+	/*
+	for _, episode := range c.EpisodeList {
+		fmt.Printf("\t%s\n", episode)
+	}
+	*/
+}
+
+func fetchPodcast(podid string) {
+	xmlFile, err := os.Open(podid)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		die(1)
+	}
+
+	defer xmlFile.Close()
+
+	file, _ := ioutil.ReadAll(xmlFile)
+
+	q := Query{}
+
+	d := xml.NewDecoder(bytes.NewReader(file))
+	err = d.Decode(&q)
+	c := q.Podcast
+
+	for _,i := range c.NewFeedUrl {
+		if i.Rel == "self" {
+			url := i.Link
+			fetch(url, "tmp")
 		}
 	}
 
+
+	err = os.Remove(podid)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		die(1)
+	}
+	
+	err = os.Rename("tmp", podid)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		die(1)
+	}
+
+	return
+	/*
 	for _, episode := range c.EpisodeList {
 		fmt.Printf("\t%s\n", episode)
+	}
+	*/
+}
+
+func fetchEpisode(podid string) {
+	check(podid)
+	xmlFile, err := os.Open(podid)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		die(1)
+	}
+
+	defer xmlFile.Close()
+
+	file, _ := ioutil.ReadAll(xmlFile)
+
+	q := Query{}
+
+	d := xml.NewDecoder(bytes.NewReader(file))
+	err = d.Decode(&q)
+	c := q.Podcast
+
+	url := c.EpisodeList[0].Enclosure.Link
+	podname := path.Base(podid)
+	filename := "media/" + podname + "/" + path.Base(url)
+	fetch(url, filename)
+	return
+	/*
+	for _, episode := range c.EpisodeList {
+		fmt.Printf("\t%s\n", episode)
+	}
+	*/
+}
+
+func pull() {
+	files, err := ioutil.ReadDir("rss")
+	if err != nil {
+		fmt.Println("%v\n", err)
+		die(1)
+	}
+	
+	for _, file := range files {
+		check(file.Name())
+		fetchPodcast("rss/" + file.Name())
+		fmt.Printf("fetching %v\n", file.Name())
+		fetchEpisode("rss/" + file.Name())
+	}
+	die(0)
+}
+
+func clean(mediaid string) {
+	if mediaid == "all" {
+		cleanall()
+	}
+	err := os.Remove(mediaid)
+	if err != nil {
+		fmt.Println("%v\n", err)
+		die(1)
+	}
+	fmt.Printf("cleaning done\n")
+	die(0)
+}
+
+func cleanall() {
+	err := os.RemoveAll("media/")
+	if err != nil {
+		fmt.Println("%v\n", err)
+		die(1)
+	}
+	fmt.Printf("cleaning done\n")
+	die(0)
+}
+
+func check(podid string) {
+	_, err := os.Stat("media")
+	if os.IsNotExist(err) {
+		fmt.Printf("media doesn't exist, creating dir\n")
+		err = os.Mkdir("media", 0755)
+		if err != nil {
+			fmt.Println("%v\n", err)
+			die(1)
+		}
+	}
+	_, err = os.Stat("media/" + path.Base(podid))
+	if os.IsNotExist(err) {
+		fmt.Printf("media/%v doesn't exist, creating dir\n", path.Base(podid))
+		err = os.Mkdir("media/" + path.Base(podid), 0755)
+		if err != nil {
+			fmt.Println("%v\n", err)
+			die(1)
+		}
+	}
+	return
+}
+
+
+func main() {
+
+	if len(os.Args) < 2 {
+		fmt.Println("not enough arguments!\n")
+		usage(1)
+	}
+	
+	switch os.Args[1] {
+	case "list":
+		list()
+	case "info":
+		if len(os.Args) < 3 {
+			fmt.Println("not enough arguments!\n")
+			die(1)
+		}
+		inputFile := os.Args[2]
+		podinfo(inputFile)
+	case "fetch":
+		if len(os.Args) < 3 {
+			fmt.Println("not enough arguments!\n")
+			die(1)
+		}
+		fetchPodcast(os.Args[2])
+		fetchEpisode(os.Args[2])
+		die(0)
+	case "pull":
+		pull()
+	case "help":
+		usage(0)
+	case "clean":
+		if len(os.Args) < 3 {
+			fmt.Println("not enough arguments!\n")
+			die(1)
+		}
+		clean(os.Args[2])
+	default:
+		usage(1)
 	}
 
 	return
